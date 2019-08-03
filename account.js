a01bad08233ecdbd868bad9b5b4ce9ed908bd695
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-03 19:06:27 +0000

    Automatically backed up by Learn

--- account.js
+++ account.js
@@ -24,7 +24,7 @@ module.exports = function account() {
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
   openAccount(customerAccount)
   setAccountType()
